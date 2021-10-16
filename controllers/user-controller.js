484e63173e5b95f126d71f376177b59d8b1cd7cb
Brian <bgswilde@gmail.com>
2021-10-16 14:30:38 -0500

    working get by id, update, and delete user functions

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -2,7 +2,7 @@ const { User } = require('../models');
 
 const userController = {
 
-    // route to get all users
+    // GET route to get all users ... /api/users
     getAllUsers(req, res) {
         User.find({})
             .select('-__v')
@@ -13,10 +13,64 @@ const userController = {
                 res.status(400).json(err);
             });
     },
+    // POST route to add a user ... /api/users
     addUser({ body }, res) {
         User.create(body)
             .then(dbUserData => res.json(dbUserData))
             .catch(err => res.json(err));
+    },
+    // GET route to get a single user by id ... /api/users/:id
+    getUserById({ params }, res) {
+        User.findOne({ _id: params.id })
+            // .populate({
+            //     path: 'thoughts',
+            //     select: '-__v'
+            // })
+            .select('-__v')
+            .then(dbUserData => {
+                // Send a 404 message if no user exists with the specified id
+                if (!dbUserData) {
+                    res.status(404).json({ message: 'I think you may have the wrong info... no thinker found.'});
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    },
+    // PUT route to edit a username/email by id ... /api/users/:id
+    updateUser({ params, body }, res) {
+        User.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
+            .then(dbUserData => {
+                // Send a 404 message if no user exists with the specified id
+                if (!dbUserData) {
+                    res.status(404).json({ message: 'I think you may have the wrong info... no thinker found.'});
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    },
+    // DELETE route to remove by id ... /api/users/:id
+    removeUser({ params }, res) {
+        User.findOneAndDelete({ _id: params.id })
+            .then(dbUserData => {
+                // Send a 404 message if no user exists with the specified id
+                if (!dbUserData) {
+                    res.status(404).json({ message: 'I think you may have the wrong info... no thinker found.'});
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
     }
 };
 
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,10 +1,16 @@
 const router = require('express').Router();
-const { getAllUsers, addUser } = require('../../controllers/user-controller');
+const { getAllUsers, addUser, getUserById, updateUser, removeUser } = require('../../controllers/user-controller');
 
 // All user routes using the same endpoint of /api/users
 router
     .route('/')
     .get(getAllUsers)
     .post(addUser);
+
+router
+    .route('/:id')
+    .get(getUserById)
+    .put(updateUser)
+    .delete(removeUser);
 
 module.exports = router;
